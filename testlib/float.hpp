d0fa11f534c6d259ce810849c2249882afc089d7
Alexey Dmitriev <riadwaw@gmail.com>
2012-10-03 20:51:23 +0400

    Change error messaging

--- testlib/float.hpp
+++ testlib/float.hpp
@@ -12,38 +12,38 @@ class FloatReader : public Reader<T> {
 public:
 	T read(IStream& stream) {
 		std::string token = stream.readToken();
-		return toDouble(token);
+		return toFloat(token);
 	}
 	T read(IStream& stream, T min, T max) {
 		T result = read(stream);
 		if(result < min + EPS || result > max - EPS)
-			throw ReadingException(Verdict::WA, "Double violates the range [" + toString(min) + "," + toString(max) + "]");
+			throw ReadingException(Verdict::WA, "Float violates the range [" + toString(min) + "," + toString(max) + "]");
 		return result;
 	}
 	T read(IStream& stream, T min, T max, std::string name) {
 		T result = read(stream);
 		if(result < min + EPS || result > max - EPS)
-			throw ReadingException(Verdict::WA, "Double " + toPrint(name) + " violates the range [" + toString(min) + "," + toString(max) + "]");
+			throw ReadingException(Verdict::WA, "Float " + toPrint(name) + " violates the range [" + toString(min) + "," + toString(max) + "]");
 		return result;
 	}
 
-	T toDouble(const std::string& input) {
+	T toFloat(const std::string& input) {
 
 		std::string usedValue = input;
 		bool negative = false;
-		if (input.size() >= MAX_DOUBLE_LENGTH)
-			throw ReadingException(Verdict::PE, expectation("Double", input));
+		if (input.size() >= MAX_LENGTH)
+			throw ReadingException(Verdict::PE, expectation("Float", input));
 		if(input[0] == '-') {
 			negative = true;
 			usedValue = usedValue.substr(1);
 		}
 		if(usedValue.empty())
-			throw ReadingException(Verdict::PE, expectation("Double", input));
+			throw ReadingException(Verdict::PE, expectation("Float", input));
 
 		if(usedValue[0] == '0' && usedValue.size() > 1 && usedValue[1] != '.')
-			throw ReadingException(Verdict::PE, expectation("Double", input));
+			throw ReadingException(Verdict::PE, expectation("Float", input));
 		if(usedValue[0] == '.')
-			throw ReadingException(Verdict::PE, expectation("Double", input));
+			throw ReadingException(Verdict::PE, expectation("Float", input));
 
 		bool wasPoint = false;
 		T integerPart = 0, fractionalPart = 0;
@@ -52,12 +52,12 @@ public:
 		for (char digit: usedValue) {
 			if(digit == '.') {
 				if (wasPoint)
-					throw ReadingException(Verdict::PE, expectation("Double", input));
+					throw ReadingException(Verdict::PE, expectation("Float", input));
 				wasPoint = true;
 			}
 			else {
 				if(digit < '0' || digit > '9')
-					throw ReadingException(Verdict::PE, expectation("Double", input));
+					throw ReadingException(Verdict::PE, expectation("Float", input));
 				if (wasPoint) {
 					fractionMultiplyer /= 10.0;
 					fractionalPart += (digit - '0') * fractionMultiplyer;
@@ -78,7 +78,7 @@ public:
 	}
 private:
 	static constexpr T EPS = 1e-10;
-	static constexpr size_t MAX_DOUBLE_LENGTH = 50;
+	static constexpr size_t MAX_LENGTH = 50;
 };
 
 template<typename T>
