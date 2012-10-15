faea5729b252e732c9977fd4ce2744782feaf6e0
Alexey Dmitriev <riadwaw@gmail.com>
2012-10-16 02:50:55 +0400

    Add possibility to disallow lower or upper case letters By default, allow only lower case in strict mode and any case in nonstrict mode

--- testlib/int.hpp
+++ testlib/int.hpp
@@ -12,9 +12,17 @@ template <typename T, int radix = 10>
 class IntegerReader : public Reader<T>{
 	static_assert(radix >= 2 && radix <= 36, "Radix must be in range [2..36]");
 public:
+	enum class Case {
+		DEFAULT,
+		LOWER,
+		UPPER,
+		BOTH,
+	};
+	explicit IntegerReader(Case allowedCase = Case::DEFAULT): allowedCase(allowedCase) {}
 	T read(IStream& stream) const {
+		Case streamCase = effectiveCase(stream);
 		std::string token = stream.readToken();
-		return toInt(token);
+		return toInt(token, streamCase);
 	}
 	T read(IStream& stream, T min, T max) const {
 		T result = read(stream);
@@ -29,6 +37,12 @@ public:
 		return result;
 	}
 private:
+	Case effectiveCase(IStream& stream) const {
+		if(allowedCase != Case::DEFAULT)
+			return allowedCase;
+		return stream.getMode() == IStream::Mode::STRICT ? Case::LOWER : Case::BOTH;
+	}
+	Case allowedCase;
 	std::vector<T> absToArray(T value) const {
 		bool negative = std::less<T>()(value, 0);
 		std::vector<T> result;
@@ -43,7 +57,7 @@ private:
 		return result;
 	}
 
-	T toInt(const std::string& input) const {
+	T toInt(const std::string& input, Case streamCase) const {
 		bool is_signed = std::numeric_limits<T>::is_signed;
 		if(!is_signed && input[0] == '-')
 			throw ReadingException(Verdict::PE, expectation("Unsigned integer", input));
@@ -58,7 +72,7 @@ private:
 		std::vector<T> digits(usedValue.length());
 		
 		for(size_t i = 0; i < usedValue.length(); ++i){
-			digits[i] = digitValue(usedValue[i]);
+			digits[i] = digitValue(usedValue[i], streamCase);
 			if(digits[i] >= radix)
 				throw ReadingException(Verdict::PE, expectation("Digit in radix " + toString(radix), usedValue[i]));
 		}
@@ -93,14 +107,22 @@ private:
 		
 	}
 	
-	T digitValue(char c) const {
+	T digitValue(char c, Case streamCase) const {
 		if(c >= '0' && c <= '9')
 			return c - '0';
-		if(c >= 'a' && c <= 'z')
+		if(lowerAllowed(streamCase) && c >= 'a' && c <= 'z')
 			return c - 'a' + 10;
-		if(c >= 'A' && c <= 'Z')
+		if(upperAllowed(streamCase) && c >= 'A' && c <= 'Z')
 			return c - 'A' + 10;
 		throw ReadingException(Verdict::PE, expectation("Digit", c));
+	}
+	
+	bool lowerAllowed(Case _case) const {
+		return _case == Case::LOWER || _case == Case::BOTH;
+	}
+	
+	bool upperAllowed(Case _case) const {
+		return _case == Case::UPPER || _case == Case::BOTH;
 	}
 };
 
