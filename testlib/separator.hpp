48e00b4d1dcb11523c4a415a00c4d2892a97cf94
Alexey Dmitriev <riadwaw@gmail.com>
2012-10-24 22:24:16 +0400

    remove std::move

--- testlib/separator.hpp
+++ testlib/separator.hpp
@@ -7,7 +7,7 @@ private:
 public:
 	/*implicit*/ Separator(char c): separator(1, c){}
 	/*implicit*/ Separator(const char* s): separator(s){}
-	/*implicit*/ Separator(const std::string& s): separator(std::move(s)){}
+	/*implicit*/ Separator(const std::string& s): separator(s){}
 	void read(IStream& stream) const {
 		for(char c: separator){
 			stream.readChar(c);
