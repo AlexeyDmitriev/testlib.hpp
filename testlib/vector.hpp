3af6a85d1db20cd9e5abd50a8326cfa5e4c23c8d
Alexander Mashrabov <amashrabov@gmail.com>
2012-10-26 11:43:41 +0400

    Little fixes in vector.hpp

--- testlib/vector.hpp
+++ testlib/vector.hpp
@@ -21,6 +21,7 @@ public:
 	template <typename Reader = DefaultReader<T>>
 	if_reader<Reader, T, type> read(IStream& stream, size_t numberElements, const Separator& separator, Reader reader = DefaultReader<T>()){
 		std::vector<T> res;
+		res.reserve(numberElements);
 		for (size_t i = 0; i < numberElements; i++){
 			res.push_back(stream.read<T>(reader));
 			if (i != numberElements - 1)
@@ -35,10 +36,10 @@ public:
 	}
 	
 	template <typename Reader = DefaultReader<T>>
-	if_reader<Reader, T, type> read(IStream& stream, const Separator& separator1, const Separator& separator2, Reader reader = DefaultReader<T>()){
+	if_reader<Reader, T, type> read(IStream& stream, const Separator& sizeSeparator, const Separator& elementsSeparator, Reader reader = DefaultReader<T>()){
 		size_t numberElements =  stream.read<size_t>();
-		separator1.read(stream);
-		return read(stream, numberElements, separator2, reader);
+		sizeSeparator.read(stream);
+		return read(stream, numberElements, elementsSeparator, reader);
 	}
 	
 private:
