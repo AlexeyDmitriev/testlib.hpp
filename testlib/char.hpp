d417e456fd85152cd0c507409d7e1b9d197db519
Alexey Dmitriev <riadwaw@gmail.com>
2012-11-22 14:41:16 +0400

    Add DefaultReader<char>

---
+++ testlib/char.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <type_traits>
+#include "reader.hpp"
+#include "istream.hpp"
+template <typename T>
+struct is_char : public std::false_type{};
+
+template <>
+struct is_char<char> : public std::true_type{};
+
+template <>
+struct is_char<signed char> : public std::true_type{};
+
+template <>
+struct is_char<unsigned char> : public std::true_type{};
+
+
+template<typename T>
+class DefaultReader<T, typename is_char<T>::type> : Reader<T>{
+public:
+	T read(IStream& stream){
+		return stream.readChar();
+	}
+	T read(IStream& stream, const std::string& allowed){
+		T result = read(stream);
+		for(char c: allowed)
+			if(result == c)
+				return result;
+		stream.quit(Verdict::PE, expectation("One of \"" + allowed + "\"", result));
+		assert(false);
+	}
+};
