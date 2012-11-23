88b5c9dfffc35e819708dcf113bf40ae28c07784
Alexey Dmitriev <riadwaw@gmail.com>
2012-11-24 00:07:15 +0400

    Move verdict macro to another file

--- testlib/macro.hpp
+++ testlib/macro.hpp
@@ -128,20 +128,3 @@ int main(){ \
 	return exitCode(verdict); \
 } \
 void validate(IStream& inf)
-
-#define QUIT(verdict, msg) \
-do { \
-	std::stringstream ss; \
-	ss << msg; \
-	throw ReadingException(verdict, ss.str()); \
-} \
-while (false); \
-
-#define OK(msg) QUIT(Verdict::OK, msg)
-#define WA(msg) QUIT(Verdict::WA, msg)
-#define FAIL(msg) QUIT(Verdict::FAIL, msg)
-#define PE(msg) QUIT(Verdict::PE, msg)
-
-#define ensure(condition) \
-if(!(condition)) \
-	FAIL("\"" #condition "\" on line "  << __LINE__ << " is false");
