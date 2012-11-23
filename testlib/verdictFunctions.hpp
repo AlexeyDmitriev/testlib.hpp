a667080dc70aa792f7c474038f08405dde4f2a42
Alexey Dmitriev <riadwaw@gmail.com>
2012-11-24 02:36:45 +0400

    Add  simple verify functions
    verifyEqual(a, b)
    verifySorted(Range)

--- testlib/verdictFunctions.hpp
+++ testlib/verdictFunctions.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <sstream>
+#include <algorithm>
 #include "core.hpp"
+#include "utility.hpp"
 #define QUIT(verdict, msg) \
 do { \
 	std::stringstream ss; \
@@ -15,5 +17,21 @@ while (false); \
 #define PE(msg) QUIT(Verdict::PE, msg)
 
 #define ensure(condition) \
-if(!(condition)) \
-	FAIL("\"" #condition "\" on line "  << __LINE__ << " is false");
+if(!(condition)) {\
+	FAIL("\"" #condition "\" on line "  << __LINE__ << " is false"); \
+}
+
+#define verify(condition, verdict, message) \
+if(!(condition)) {\
+	QUIT(verdict, message); \
+}
+
+template <typename T, typename U>
+inline void verifyEqual(T&& t, U&& u, Verdict verdict = Verdict::WA){
+	verify(t == u, verdict, t << " != " << u);
+}
+
+template <typename T>
+inline void verifySorted(T start, T end, Verdict verdict = Verdict::WA){
+	verify(std::is_sorted(start, end), verdict, expectation("Sorted range", rangeToString(start, end)));
+}
