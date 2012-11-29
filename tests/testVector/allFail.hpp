adf94a9ae66710a6e06011fd82fb070876534138
Alexander Mashrabov <amashrabov@gmail.com>
2012-11-29 14:14:15 +0400

    Tests for vector have been changed

--- tests/testVector/allFail.hpp
+++ tests/testVector/allFail.hpp
@@ -7,7 +7,7 @@ BOOST_AUTO_TEST_CASE(numberElementsThrow){
 	BOOST_CHECK_THROW(stream.read<vi>(), ReadingException);
 }
 
-BOOST_AUTO_TEST_CASE(testAllMethodsThrow){
+BOOST_AUTO_TEST_CASE(vectorNumElemThrow){		
 	setStr("12 10 1k"); //+
 	vi res = {12, 10, 1};
 	size_t q = 3;
@@ -16,30 +16,34 @@ BOOST_AUTO_TEST_CASE(testAllMethodsThrow){
 	setStr("p12 10 1"); //+
 	res = {12, 10, 1};
 	BOOST_CHECK_THROW(stream.read<vi>(q, DefaultReader<int>()), ReadingException);
-	
+}
+
+BOOST_AUTO_TEST_CASE(vectorNumElemSeparatorThrow){
 	setStr("12\t10 1"); //+
-	res = {12, 10, 1};
+	vi res = {12, 10, 1};
 	BOOST_CHECK_THROW(stream.read<vi>(3, sepSpace), ReadingException);
 	
 	setStr("12 10\n1"); //+
 	res = {12, 10, 1};
 	BOOST_CHECK_THROW(stream.read<vi>((long long)(3), sepSpace, DefaultReader<int>()), ReadingException);	
-	
+}
+
+BOOST_AUTO_TEST_CASE(vectorEmptyThrow){
 	setStr("2vs12 -1"); //+
-	res = {12, -1};
+	vi res = {12, -1};
 	BOOST_CHECK_THROW(stream.read<vi>(), ReadingException);
 	
 	setStr("3 12 10teorver1"); //+
 	res = {3, 12, 10};
 	BOOST_CHECK_THROW(stream.read<vi>(DefaultReader<int>()), ReadingException);
-	
-	res = {12, -1};
+}
+
+
+BOOST_AUTO_TEST_CASE(vectorSeparatorThrow){	
+	vi res = {12, -1};
 	setStr("2\n12 -1"); //+
 	BOOST_CHECK_THROW(stream.read<vi>(sepTab, sepSpace), ReadingException);
-	setStr("2\n12 -1"); //+
-	BOOST_CHECK_THROW(stream.read<vi>(sepTab, sepSpace), ReadingException);
-	
+	res = {12, 10, -1};
 	setStr("3\n12 10 1"); //+
 	BOOST_CHECK_THROW(stream.read<vi>(sepTab, sepSpace, DefaultReader<int>()), ReadingException);
-	
 }
--- tests/testVector/allOk.hpp
+++ tests/testVector/allOk.hpp
@@ -1,50 +1,46 @@
-BOOST_AUTO_TEST_CASE(numberElements){
-	setStr("1.0 1.0");
-	BOOST_CHECK_THROW(stream.read<vd>(), ReadingException);
-	
-	setStr("-1");
-	vi res = {-1};
-	BOOST_CHECK_THROW(stream.read<vi>(), ReadingException);
-	
+BOOST_AUTO_TEST_CASE(numberElements){	
 	setStr("3\n12 10 1");
-	res = {12, 10, 1};
+	vi res = {12, 10, 1};
 	BOOST_CHECK(stream.read<vi>(DefaultReader<int>()) == res);
 }
 
-BOOST_AUTO_TEST_CASE(testAllMethods){	
+BOOST_AUTO_TEST_CASE(vectorNumElem){		
+	setStr("2\n12 -1"); 
 	vi res = {12, -1};
-	setStr("12 -1"); //+
-	BOOST_CHECK(stream.read<vi>(2) == res);
-	
-	setStr("12 10 1"); //+
-	res = {12, 10, 1};
-	BOOST_CHECK(stream.read<vi>((unsigned int)(3), DefaultReader<int>()) == res);
-	
-	setStr("12\t10\t1"); //+
-	res = {12, 10, 1};
-	BOOST_CHECK(stream.read<vi>((long long)(3), sepTab) == res);
-	
-	setStr("12 10 1"); //+
-	res = {12, 10, 1};
-	BOOST_CHECK(stream.read<vi>((long long)(3), sepSpace, DefaultReader<int>()) == res);
-	
-	setStr("2\n12 -1"); //+
-	res = {12, -1};
 	BOOST_CHECK(stream.read<vi>() == res);
 	
-	setStr("2\n12 -1"); //+
+	setStr("2\n12 -1"); 
 	res = {12, -1};
 	BOOST_CHECK(stream.read<vi>(DefaultReader<int>()) == res);
-	
-	setStr("2\t12 -1"); //+
-	res = {12, -1};
+}
+
+BOOST_AUTO_TEST_CASE(vectorNumElemSeparator){
+	setStr("2\t12 -1");
+	vi res = {12, -1};
 	BOOST_CHECK(stream.read<vi>(sepTab, sepSpace) == res);
 	
-	setStr("3\t12 10 1"); //+
+	setStr("3\t12 10 1");
 	res = {12, 10, 1};
 	BOOST_CHECK(stream.read<vi>(sepTab, sepSpace, DefaultReader<int>()) == res);
 }
 
-BOOST_AUTO_TEST_CASE(Separator){
+BOOST_AUTO_TEST_CASE(vectorEmpty){
+	vi res = {12, -1};
+	setStr("12 -1"); //+
+	BOOST_CHECK(stream.read<vi>(2) == res);
 	
+	setStr("12 10 1");
+	res = {12, 10, 1};
+	BOOST_CHECK(stream.read<vi>((unsigned int)(3), DefaultReader<int>()) == res);
+}
+
+
+BOOST_AUTO_TEST_CASE(vectorSeparator){	
+	setStr("12\t10\t1");
+	vi res = {12, 10, 1};
+	BOOST_CHECK(stream.read<vi>((long long)(3), sepTab) == res);
+	
+	setStr("12 10 1"); 
+	res = {12, 10, 1};
+	BOOST_CHECK(stream.read<vi>((long long)(3), sepSpace, DefaultReader<int>()) == res);
 }
