6dc9b7a6e11aa05eb31a2737099900304820856f
Alexey Dmitriev <riadwaw@gmail.com>
2012-11-30 17:22:44 +0400

    Rewrite Alias to derive aliased type
     #9

--- testlib/alias.hpp
+++ testlib/alias.hpp
@@ -1,18 +1,32 @@
 #pragma once
 #include "reader.hpp"
 #include <type_traits>
+#include <iostream>
 
-template <typename T, typename R>
-class Alias {
+template <typename T, typename R, typename X>
+class AliasImpl;
+
+template<typename T, typename R>
+class AliasImpl<T, R, std::true_type>{
 	static_assert(std::is_base_of<Reader<T>, R>::value, "R must be reader of T");
 public:
-	/*implicit*/ Alias(const T& value): value(value){}
+	/*implicit*/ AliasImpl(const T& value): T(value){}
+};
+
+template <typename T, typename R>
+class AliasImpl<T, R, std::false_type>{
+	static_assert(std::is_base_of<Reader<T>, R>::value, "R must be reader of T");
+public:
+	/*implicit*/ AliasImpl(const T& value): value(value){}
 	operator T () const {
 		return value;
 	}
 private:
 	T value;
 };
+
+template <typename T, typename R>
+using Alias = AliasImpl<T, R, typename std::is_class<T>::type>;
 
 template <typename T, typename R>
 class DefaultReader<Alias<T,R>> : public Reader<Alias<T,R>> {
