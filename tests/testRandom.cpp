60cdffb7810f26c70590a8495210369573add083
Alexey Dmitriev <riadwaw@gmail.com>
2013-03-04 20:44:03 +0400

    Test shuffle
    Affects #15

--- tests/testRandom.cpp
+++ tests/testRandom.cpp
@@ -1,5 +1,7 @@
 #include "testTools.hpp"
 #include <boost/test/unit_test.hpp>
+#include <vector>
+#include <set>
 #include "testlib/verdictFunctions.hpp"
 #include "testlib/random.hpp"
 #include "testlib/generators/int.hpp"
@@ -25,6 +27,25 @@ BOOST_AUTO_TEST_CASE(customGenerator) {
 	for(int i = 0; i < 10; ++i){
 		BOOST_CHECK_EQUAL(rnd.next<int>(ones), 1);
 	}
+}
+
+BOOST_AUTO_TEST_CASE(shuffleTest) {
+	std::vector<int> v;
+	std::multiset<int> valuesBefore, valuesAfter;
+	const int N = 100;
+	for(int i = 0; i < N; ++i){
+		int value = rnd.next<int>();
+		v.push_back(value);
+		valuesBefore.insert(value);
+	}
+
+	rnd.shuffle(v.begin(), v.end());
+
+	for(int value: v){
+		valuesAfter.insert(value);
+	}
+
+	BOOST_CHECK_EQUAL_COLLECTIONS(valuesBefore.begin(), valuesBefore.end(), valuesAfter.begin(), valuesAfter.end());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
