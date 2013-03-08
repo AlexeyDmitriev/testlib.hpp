3f15fbbf18ab8489377e7e1f34b8f8de767dad4f
Alexey Dmitriev <riadwaw@gmail.com>
2013-03-08 12:28:53 +0400

    generate<vector>(int) -> generate<vector>(size_t) #15

--- testlib/generators/vector.hpp
+++ testlib/generators/vector.hpp
@@ -4,10 +4,10 @@ template<typename T>
 class DefaultGenerator<std::vector<T>> : Generator<std::vector<T>> {
 public:
 	template<typename... Args>
-	std::vector<T> generate(Random& rnd, int n, Args&&... args){
+	std::vector<T> generate(Random& rnd, size_t n, Args&&... args){
 		std::vector<T> result;
 		result.reserve(n);
-		for(int i = 0; i < n; ++i){
+		for(size_t i = 0; i < n; ++i){
 			result.push_back(rnd.next<T>(std::forward<Args>(args)...));
 		}
 		return result;
