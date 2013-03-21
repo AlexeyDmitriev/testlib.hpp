cde8f1cdcd12e7555e06ba4bd70aa9933e812ce7
Alexey Dmitriev <riadwaw@gmail.com>
2013-03-21 23:45:42 +0400

    Add example generator #15

--- examples/generators/array.cpp
+++ examples/generators/array.cpp
@@ -2,5 +2,7 @@
 
 TESTLIB_GENERATE() {
 	int n = atoi(args[0].c_str());
+	int max = atoi(args[1].c_str());
 	std::cout << n << std::endl;
+	std::cout << separated(rnd.next<std::vector<int>>(n, 1, max), ' ') << std::endl;
 }
