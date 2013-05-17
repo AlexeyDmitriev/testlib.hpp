ddf29aefdf75d1a2fbdd94ff441f59cc3ba60559
Andrey Kashin <iiotep9huy@gmail.com>
2013-05-17 12:20:55 +0400

    UnorderedPair generator tests changed

--- tests/testUnorderedPair/generatorTests.hpp
+++ tests/testUnorderedPair/generatorTests.hpp
@@ -1,9 +1,22 @@
+#include "testlib/generatorWrapper.hpp"
+
 BOOST_FIXTURE_TEST_SUITE(UnorderedPairsRandom, RandomTest)
 
 BOOST_AUTO_TEST_CASE(simpleGenerate) {
   UniqueGenerator<std::vector<pi>> generator;
-  auto pairs = generator.generate(rnd, 10);
+  const int N = 150;
+  const int minNumber = -10;
+  const int maxNumber = 10;
+  auto intGenerator = make_generator(DefaultGenerator<int>(), minNumber, maxNumber);
+  auto pairGenerator = make_generator(DefaultGenerator<pi>(), intGenerator);
+  auto pairs = generator.generate(rnd, N, pairGenerator);
+  
   BOOST_CHECK(std::unique(pairs.begin(), pairs.end()) == pairs.end());
+
+  for (auto pair : pairs) {
+    BOOST_CHECK(pair.first >= minNumber && pair.first <= maxNumber
+              && pair.second >= minNumber && pair.second <= maxNumber);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
