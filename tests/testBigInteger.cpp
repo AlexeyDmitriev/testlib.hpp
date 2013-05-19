e2c4b338f6b0346167ea394f24177f0b302f70c7
Alexander Mashrabov <amashrabov@gmail.com>
2013-05-19 19:40:15 +0400

    Add BigInteger %= (int); avoid overflow in *,/; tests with zero; ban recursion in +

--- tests/testBigInteger.cpp
+++ tests/testBigInteger.cpp
@@ -2,6 +2,9 @@
 #include "libs/BigInteger/BigInteger.hpp"
 #include "tests/testReadTools.hpp"
 #include "testRandomTools.hpp"
+#include "testlib/utility.hpp"
+#include <vector>
+#include <string>
 
 BOOST_FIXTURE_TEST_SUITE(BigIntegerStrict, StrictRead)
 
@@ -35,30 +38,34 @@ BOOST_AUTO_TEST_CASE(BIgcd) {
 }
 
 BOOST_AUTO_TEST_CASE(arithm) {
-	std::vector<int> a = {1,  5, 3, -5, -1, -2, -5, -6, 5,  0, 0};
-	std::vector<int> b = {2, -3, -5, 3,  1, -6,  8, -2, 4, -5, 7};
-	BOOST_REQUIRE_EQUAL(a.size(), b.size());
-	for(size_t i = 0; i < a.size(); ++i) {
-		BOOST_CHECK_EQUAL(BigInteger(a[i]) + BigInteger(b[i]), BigInteger(a[i] + b[i]));
-	}
-	for(size_t i = 0; i < a.size(); ++i) {
-		BOOST_CHECK_EQUAL(BigInteger(a[i]) - BigInteger(b[i]), BigInteger(a[i] - b[i]));
-	}
-	for(size_t i = 0; i < a.size(); ++i) {
-		BOOST_CHECK_EQUAL(BigInteger(a[i]) * BigInteger(b[i]), BigInteger(a[i] * b[i]));
-	}
-	for(size_t i = 0; i < a.size(); ++i) {
-		BOOST_CHECK_EQUAL(BigInteger(a[i]) / BigInteger(b[i]), BigInteger(a[i] / b[i]));
-	}
-
-	for(size_t i = 0; i < a.size(); ++i) {
-		BOOST_CHECK_EQUAL(BigInteger(a[i]) % BigInteger(b[i]), BigInteger(a[i] % b[i]));
+	for(int i = -113; i < 113; i += 3) {
+		for (int j = -215; j < 215; j += 2) {
+			BOOST_CHECK_EQUAL((BigInteger(i) + BigInteger(j)).toString(), toString(i + j));
+			BOOST_CHECK_EQUAL((BigInteger(i) - BigInteger(j)).toString(), toString(i - j));
+			BOOST_CHECK_EQUAL((BigInteger(i) * BigInteger(j)).toString(), toString(i * j));
+			BOOST_CHECK_EQUAL((BigInteger(i) / BigInteger(j)).toString(), toString(i / j));
+			BOOST_CHECK_EQUAL((BigInteger(i) % BigInteger(j)).toString(), toString(i % j));
+			BOOST_CHECK_EQUAL(gcd(BigInteger(i), BigInteger(j)), BigInteger(std::__gcd(i, j)).abs());
+		}
 	}
 }
 
-BOOST_AUTO_TEST_CASE(zeroDivision) {
-	BigInteger a{"47743248374387483247832"};
-	BOOST_CHECK_THROW(a / BigInteger::zero(), VerdictException);
-	BOOST_CHECK_THROW(a % BigInteger::zero(), VerdictException);
+BOOST_AUTO_TEST_CASE(zeroOperations) {
+	std::vector<std::string> arr = {"47743248374387483247832", "-47743248374387483247832", "0"};
+	for (auto s  : arr){
+		BigInteger a(s);
+		BOOST_CHECK_EQUAL(a + BigInteger::zero(), a);
+		BOOST_CHECK_EQUAL(a - BigInteger::zero(), a);
+		BOOST_CHECK_EQUAL(BigInteger::zero() + a, a);
+		BOOST_CHECK_EQUAL(BigInteger::zero() - a, -a);
+		BOOST_CHECK_EQUAL(a * BigInteger::zero(), BigInteger::zero());
+		BOOST_CHECK_EQUAL(BigInteger::zero() * a, BigInteger("0"));
+		BOOST_CHECK_EQUAL(pow(a, 0), BigInteger::one());
+		BOOST_CHECK_EQUAL(gcd(a, BigInteger(0)), a.abs());
+		BOOST_CHECK_EQUAL(gcd(BigInteger(0), a), a.abs());
+		
+		BOOST_CHECK_THROW(a / BigInteger::zero(), VerdictException);
+		BOOST_CHECK_THROW(a % BigInteger::zero(), VerdictException);
+	}
 }
 BOOST_AUTO_TEST_SUITE_END()
