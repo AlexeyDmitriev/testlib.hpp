ea4ffd1c344743bd364266a817df9891c857f615
Alexander Mashrabov <amashrabov@gmail.com>
2013-05-19 22:48:18 +0400

    New tests with arith operations with BigInt

--- tests/testBigInteger.cpp
+++ tests/testBigInteger.cpp
@@ -13,31 +13,65 @@ BOOST_AUTO_TEST_CASE(BigIntegerRead){
 	BOOST_CHECK_EQUAL(stream.read<BigInteger>().toInteger(), -894);
 }
 
+BOOST_AUTO_TEST_CASE(BadFormat) {
+	std::vector<std::string> strings = {
+		"--1",
+		"+1",
+		"1-2",
+		"za",
+		"123a123",
+		"01",
+		"-0",
+		"-12a6",
+		"-a1",
+		"-9a9",
+		"-01",
+		"00",
+		"0.0",
+		"30.",
+		"-.0",
+		"-.2",
+		"-2.0",
+		".1",
+		"2.2",
+		"",
+	};
+	
+	for (const auto& s : strings) {
+		setStr(s);
+		BOOST_CHECK_THROW(stream.read<BigInteger>(), VerdictException);
+	}
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_FIXTURE_TEST_SUITE(BigIntegerRandom, RandomTest)
 
 BOOST_AUTO_TEST_CASE(BigIntegerRandom){
-	BigInteger from = BigInteger(010100);
-	BigInteger to = BigInteger(100100);
-	BigInteger r = rnd.next<BigInteger>(from, to);
-	BOOST_CHECK(from <= r && r <= to);
+	for (int i = 0; i < 1000; i++) {
+		BigInteger from = BigInteger(010100);
+		BigInteger to = BigInteger(100100);
+		BigInteger r = rnd.next<BigInteger>(from, to);
+		BOOST_CHECK(from <= r && r <= to);
+	}
 }
 
-BOOST_AUTO_TEST_CASE(BIgcd) {
-	{
-		BigInteger a(3);
-		BigInteger b(5);
-		BOOST_CHECK_EQUAL(gcd(a, b), BigInteger(1));
-	}
+BOOST_AUTO_TEST_CASE(BIcompare) {
+	for (int i = -1011; i < 2018; i+=73)
+		for (int j = -666; j < 113; j += 37) {
+			BOOST_CHECK_EQUAL(BigInteger(i) < BigInteger(j), i < j);
+			BOOST_CHECK_EQUAL(BigInteger(i) > BigInteger(j), i > j);
+			BOOST_CHECK_EQUAL(BigInteger(i) >= BigInteger(j), i >= j);
+			BOOST_CHECK_EQUAL(BigInteger(i) <= BigInteger(j), i <= j);
+		}
+}
+
+BOOST_AUTO_TEST_CASE(arithmOperations) {
 	{
 		BigInteger a("10394540081827693200473815328053988703889568");
 		BigInteger b("10008658978277140167971344646087256144407296");
 		BOOST_CHECK_EQUAL(gcd(a, b), BigInteger("2669734086172442650208"));
 	}
-}
-
-BOOST_AUTO_TEST_CASE(arithm) {
 	for(int i = -113; i < 113; i += 3) {
 		for (int j = -215; j < 215; j += 2) {
 			BOOST_CHECK_EQUAL((BigInteger(i) + BigInteger(j)).toString(), toString(i + j));
@@ -46,6 +80,7 @@ BOOST_AUTO_TEST_CASE(arithm) {
 			BOOST_CHECK_EQUAL((BigInteger(i) / BigInteger(j)).toString(), toString(i / j));
 			BOOST_CHECK_EQUAL((BigInteger(i) % BigInteger(j)).toString(), toString(i % j));
 			BOOST_CHECK_EQUAL(gcd(BigInteger(i), BigInteger(j)), BigInteger(std::__gcd(i, j)).abs());
+			BOOST_CHECK_EQUAL(lcm(BigInteger(i), BigInteger(j)).abs(), BigInteger(i * j / std::__gcd(i, j)).abs());
 		}
 	}
 }
@@ -68,4 +103,5 @@ BOOST_AUTO_TEST_CASE(zeroOperations) {
 		BOOST_CHECK_THROW(a % BigInteger::zero(), VerdictException);
 	}
 }
+
 BOOST_AUTO_TEST_SUITE_END()
